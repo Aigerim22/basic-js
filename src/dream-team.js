22824fd574ce7c149335bee3b067f8d59e389410
Aigerim Tazhibay <aika0822t@gmail.com>
2021-02-09 23:49:12 +0600

    feat: add 3 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      let removed = members[i].trim();
+      arr.push(removed.charAt(0).toUpperCase());
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    return arr.sort().join("");
+  }
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  var seasons = ["spring", "summer", "autumn", "winter"];
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  for (let elem in date) {
+    if (typeof date[elem] != "number") {
+      throw new Error("Error");
+    }
+  }
+  if (date.getMonth() == 2 || date.getMonth() == 3 || date.getMonth() == 4)
+    return seasons[0];
+  if (date.getMonth() == 5 || date.getMonth() == 6 || date.getMonth() == 7)
+    return seasons[1];
+  if (date.getMonth() == 8 || date.getMonth() == 9 || date.getMonth() == 10)
+    return seasons[2];
+  if (date.getMonth() == 11 || date.getMonth() == 0 || date.getMonth() == 1)
+    return seasons[3];
 };
